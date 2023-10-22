44a146db0b1b8f6304609f4bc55052c4373bf029
Anita Hegde <hegde.anit@northeastern.edu>
2023-10-22 10:28:17 -0700

    adding Stock app

---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Anita Hegde on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "d15e9a346e61cf18ffdaead9a4e1fee5"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
